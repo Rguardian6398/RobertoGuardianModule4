43a86869e500dadc1a54d301650f010b0453f607
Roberto Guardian <77365086+Rguardian6398@users.noreply.github.com>
2021-01-20 22:23:38 -0700

    Add files via upload
    Module 4 Assignments

---
+++ Coding Assignments/RobertoGuardianModule4/RobertoGuardianModule4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RobertoGuardianModule4
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime myDateTimeVariable = new DateTime(2012,06,12);
+            DateTime myDateTimeVariable2 = DateTime.Now;
+
+            int result = DateTime.Compare(myDateTimeVariable, myDateTimeVariable2);
+
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Coding Assignments/RobertoGuardianModule4/RobertoGuardianModule4/RobertoGuardian1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RobertoGuardianModule4
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime myDateTimeVariable = new DateTime(2020,12,10);
+            DateTime myDateTimeVariable2 = new DateTime(2001, 12, 10);
+
+            int result = DateTime.Compare(myDateTimeVariable, myDateTimeVariable2);
+
+
+            Console.WriteLine(result);
+        }
+    }
+}

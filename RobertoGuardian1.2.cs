5333325ad7591c89e82a5d6d4db01d91e84d5bc1
Roberto Guardian <77365086+Rguardian6398@users.noreply.github.com>
2021-01-23 12:15:14 -0700

    Add files via upload
    This is my edited module 4 assignment

---
+++ RobertoGuardian1.2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RobertoGuardianModule4
+{
+        class Program
+        {
+        static void Main()
+        {
+            DateTime myDateTimeVariable = new DateTime(2020, 12, 10);
+            DateTime myDateTimeVariable2 = new DateTime(2001, 12, 10);
+
+            int result = DateTime.Compare(myDateTimeVariable, myDateTimeVariable2);
+
+            if (result == 1)
+            {
+                Console.WriteLine("First date is greater");
+            }
+            else
+            {
+                Console.WriteLine("Second date is greater");
+            }
+        }
+        }
+    }

2867519a5de4194b78219c04dd365a9e664668bd
Roberto Guardian <77365086+Rguardian6398@users.noreply.github.com>
2021-01-23 15:19:38 -0700

    Add files via upload
    The is my 5 coding assignment

---
+++ RobertoGuardianModule5/RobertoGuardianModule5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RobertoGuardianModule5
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int x = 0; x < 21; x++)
+          {
+                Console.WriteLine("Line: " + x);
+          }
+            int sum = 21;
+
+            for (int i = 0; i <= 21; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine("The result is: " + sum);
+        }
+    }
+}

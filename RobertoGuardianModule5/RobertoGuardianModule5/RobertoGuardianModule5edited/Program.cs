3f63a92cb49f9af1a8a83825de1f5e2ec7db350d
Roberto Guardian <77365086+Rguardian6398@users.noreply.github.com>
2021-01-25 20:09:49 -0700

    Add files via upload
    Edited Module 5 assignment

---
+++ RobertoGuardianModule5/RobertoGuardianModule5/RobertoGuardianModule5edited/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RobertoGuardianModule5edited
+{
+    class Program
+    {
+        static void Main()
+        {
+            int sum = 0;
+
+            for (int i = 1; i <= 20; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine("The result is: " + sum);
+        }
+    }
+}

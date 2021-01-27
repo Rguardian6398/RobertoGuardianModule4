e58c2a46c455b24d55c3ae52dcb98b6f9ef37b38
Roberto Guardian <77365086+Rguardian6398@users.noreply.github.com>
2021-01-26 17:39:55 -0700

    Add files via upload
    This is my Module 6 assignment.

---
+++ RobertoGuardianModule6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RobertoGuardianModule6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[] {1,2,3,4,5,6,7,8,9,10};
+
+            Console.WriteLine("The array has a length of: " +
+                               myArray.Length);
+
+            //foreach(var item in myArray)
+            //{
+            //  Console.WriteLine(item);
+            //}
+        }
+    }
+}

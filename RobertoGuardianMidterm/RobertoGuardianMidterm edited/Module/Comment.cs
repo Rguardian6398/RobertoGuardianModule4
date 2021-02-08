85911ac89abfa6bf4a00c86a80d28222c5dde751
Roberto Guardian <77365086+Rguardian6398@users.noreply.github.com>
2021-02-08 16:31:37 -0700

    Add files via upload
    This is my edited midterm

---
+++ RobertoGuardianMidterm/RobertoGuardianMidterm edited/Module/Comment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace RobertoGuardianMidterm.Module
+{
+    public class Comment
+    {
+        public Comment()
+        {
+        }
+
+        public string TheComment { get; set; }
+        public int CommentId { get; set; }
+    }
+}

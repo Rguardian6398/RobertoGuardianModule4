a27aa54eb2eb2188ade426634730078f7abde881
Roberto Guardian <77365086+Rguardian6398@users.noreply.github.com>
2021-02-09 17:41:26 -0700

    Add files via upload
    My Module 8 assignment

---
+++ RobertoGuardianModule8/RobertoGuardianModule8/RobertoGuardianModule8.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RobertoGuardianModule8
+{
+    class Program
+    {
+        static void Main()
+        {
+            MyClass<int> myClass = new MyClass<int>();
+
+            myClass.MyProperty1 = 10;
+            myClass.MyProperty2 = 20;
+
+            Console.WriteLine(myClass.MyProperty1);
+            Console.WriteLine(myClass.MyProperty2);
+
+
+            MyClass<string> myClass2 = new MyClass<string>();
+
+            myClass2.MyProperty1 = "string 1";
+            myClass2.MyProperty2 = "string 2";
+
+            Console.WriteLine(myClass2.MyProperty1);
+            Console.WriteLine(myClass2.MyProperty2);
+
+        }
+    }
+}
+            public class MyClass<T>
+            { 
+              public T MyProperty1 { get; set; }
+              public T MyProperty2 { get; set; }
+            }

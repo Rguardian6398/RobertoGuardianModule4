bd11e9a5a24468e4ed46973e47d79accf696302f
Roberto Guardian <77365086+Rguardian6398@users.noreply.github.com>
2021-02-10 21:41:19 -0700

    Add files via upload
    my Module 9 assignment

---
+++ RobertoGuardianModule9/RobertoGuardianModule9/RobertoGuardianModule9.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RobertoGuardianModule9
+{
+    class Program
+    {
+        static void Main()
+        {
+            MyClass myVariable = new MyClass();
+            myVariable.MyMethod<string>("string 1", "string 2");
+
+            Console.WriteLine(myVariable.MyProperty1);
+            Console.WriteLine(myVariable.MyProperty2);
+        }
+    }
+}
+
+public class MyClass
+{
+    public string MyProperty1 { get; set; }
+    public string MyProperty2 { get; set; }
+
+    public void MyMethod<T>(T parameter1, T parameter2)
+    {
+        this.MyProperty1 = parameter1.ToString();
+        this.MyProperty2 = parameter2.ToString();
+    }
+}

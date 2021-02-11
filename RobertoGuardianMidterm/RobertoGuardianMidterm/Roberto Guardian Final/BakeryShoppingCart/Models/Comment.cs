02e2353ca5fe91e3723b2f49435366212ad6b3c6
Roberto Guardian <77365086+Rguardian6398@users.noreply.github.com>
2021-02-10 23:19:36 -0700

    Add files via upload
    My Final project

---
+++ RobertoGuardianMidterm/RobertoGuardianMidterm/Roberto Guardian Final/BakeryShoppingCart/Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BakeryShoppingCart.Models
+{
+    public class Comment
+    {
+        public Comment()
+        {
+        }
+
+        public int CommentId { get; internal set; }
+        public string TheComment { get; internal set; }
+    }
+}
